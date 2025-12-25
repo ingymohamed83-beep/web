838331a957a03cd8719a3039a85a1f0a41404928
ingymohamed83-beep <ingymohamed83@gmail.com>
2025-12-26 00:20:03 +0200

    Add files via upload

---
+++ mohamed.js
@@ -0,0 +1,25 @@
+document.querySelector('.menu-toggle').addEventListener('click', function(){
+document.querySelector('nav').classList.toggle('active');
+
+});
+
+
+function go() {
+
+    window.location.href = "login.html";
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ student.js
@@ -0,0 +1,42 @@
+function lig() {
+
+var text =document.getElementById("text");
+var email =document.getElementById("email");
+var num =document.getElementById("num");
+var pass =document.getElementById("pass");
+
+if (text.lenth===4 && email.lenth===8 && num.lenth===12 && pass.lenth===6) {
+
+window.location("student2.html");
+
+
+} else {
+    window.alert("عزيزي الطالب برجاء ادخال بياناتك ");
+}
+
+
+
+
+
+
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
